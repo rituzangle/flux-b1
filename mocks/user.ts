eb27a02db3bc0bd17cbe64a952272b5d14f7024b
Ritu Sangha <rituzangle@users.noreply.github.com>
2025-10-15 01:06:47 -0700

    Updated user.ts

--- mocks/user.ts
+++ mocks/user.ts
@@ -1,13 +1,28 @@
 import { User } from '@/utils/types';
+export const mockTransactions: Transaction[] = [
 
-export const mockUser: User = {
-  id: 'user-1',
-  name: 'Alex Maigret',
-  email: 'alex@example.com',
-  balance: 1250.00,
-  hasCompletedOnboarding: false,
-  firstDonationDate: null,
-  totalDonated: 0,
-  lastWWPromptShown: null,
-  wwPromptDismissedForPayday: false,
-};
+export const mockUser: User[] = [
+  {
+    id: 'user-1',
+    name: 'Alex Maigret',
+    email: 'alex@example.com',
+    balance: 1250.00,
+    hasCompletedOnboarding: false,
+    firstDonationDate: null,
+    totalDonated: 0,
+    lastWWPromptShown: null,
+    wwPromptDismissedForPayday: false,
+  },
+  {
+    id: 'user-2',
+    name: 'Vera Wing',
+    email: 'vera@vera.com',
+    balance: 9250.00,
+    hasCompletedOnboarding: false,
+    firstDonationDate: null,
+    totalDonated: 0,
+    lastWWPromptShown: null,
+    wwPromptDismissedForPayday: false,
+  },
+  
+];
