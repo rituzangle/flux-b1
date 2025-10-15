38b685e9d6e739d8385a6eb9bf4716b60268c3d1
Ritu Sangha <rituzangle@users.noreply.github.com>
2025-10-15 01:07:24 -0700

    Updated user.ts

--- mocks/user.ts
+++ mocks/user.ts
@@ -12,16 +12,5 @@ export const mockUser: User[] = [
     lastWWPromptShown: null,
     wwPromptDismissedForPayday: false,
   },
-  {
-    id: 'user-2',
-    name: 'Vera Wing',
-    email: 'vera@vera.com',
-    balance: 9250.00,
-    hasCompletedOnboarding: false,
-    firstDonationDate: null,
-    totalDonated: 0,
-    lastWWPromptShown: null,
-    wwPromptDismissedForPayday: false,
-  },
   
 ];
