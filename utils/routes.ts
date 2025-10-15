3ccbc049d326bc7e9ecba6062bfef1f1cdb15d35
Ritu Sangha <rituzangle@users.noreply.github.com>
2025-10-14 23:43:44 -0700

    Deleted routes.ts

--- utils/routes.ts
+++
@@ -1,7 +0,0 @@
-// utils/routes.ts
-export const ROUTES = {
-  HOME: '/',
-  ONBOARDING: '/onboarding',
-  DONATE: '/donate',
-  AMOUNT: (charityId: string) => `/onboarding/amount?charity=${charityId}`,
-};
