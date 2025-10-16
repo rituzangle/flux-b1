d934385aac84eb52c414073f37eb436154fac88c
Ritu Sangha <rituzangle@users.noreply.github.com>
2025-10-16 00:57:17 -0700

    Updated tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,9 +3,10 @@ import type { Config } from 'tailwindcss'
 const config: Config = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    '.src//components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
-    '.src//utils/**/*.{ts,tsx}',
+    './src/utils/**/*.{ts,tsx}',
+    './src/mocks/**/*.{ts,tsx}',
   ],
   theme: {
     extend: {
